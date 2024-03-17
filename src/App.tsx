9959b2c1f5518109e86f7dc25a5dde11c0546d4f
michaeltrinh7@gmail.com <@Cuctronquay123456789>
2024-03-17 11:31:01 +0100

    Add workflow files

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import '@fontsource/open-sans/300.css'; // Light
 import '@fontsource/open-sans/400.css'; // Regular
 import '@fontsource/open-sans/600.css'; // Semi-bold
